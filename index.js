f92a5b8df388592816f99c8cf61a0d9cdf07211c
Dimitar Milov <dimitarmilov91@gmail.com>
2020-04-07 22:08:02 +0300

    README

--- index.js
+++
@@ -1,7 +0,0 @@
-const { Builder, By, Key, util } = require("selenium-webdriver");
-async function examle() {
-    let driver = await new Builder().forBrowser("firefox").build();
-    await driver.get("http://google.com");
-    await driver.findElement(By.name("q")).sendKeys("abv.bg", Key.RETURN);
-}
-examle();
