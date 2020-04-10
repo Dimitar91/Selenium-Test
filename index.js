f58dcce1bc536a290974cced1bf573a8f15ae237
Dimitar Milov <dimitarmilov91@gmail.com>
2020-04-10 22:11:06 +0300

    const BROWSER

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Builder, By, Key, util } = require("selenium-webdriver");
 const BROWSER = "firefox"
 async function examle() {
-    let driver = await new Builder().BROWSER().build();
+    let driver = await new Builder().forBrowser(BROWSER).build();
     await driver.get("http://google.com");
     await driver.findElement(By.name("q")).sendKeys("abv.bg", Key.RETURN);
 }
